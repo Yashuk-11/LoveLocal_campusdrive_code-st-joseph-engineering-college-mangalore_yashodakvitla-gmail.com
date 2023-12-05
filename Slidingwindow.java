8ffa4f1f2b646fc4472d695a96af8a9edb794e55
Yashoda k <143180882+Yashuk-11@users.noreply.github.com>
2023-12-05 15:19:41 +0530

    first commit

--- Slidingwindow.java
+++ Slidingwindow.java
@@ -2,34 +2,32 @@
 
 import java.util.Scanner;
 public class Slidingwindow {
-	
 	void maxSlidingwindow(int[] nums,int k){
 		for(int i=0;i<nums.length;i++){
-            int max=nums[i];              //storing a first value in the sliding window into max
-            int m=i+k;				//to get sliding window width at each iteration
-            if(m<=nums.length){				
-                for(int j=i+1;j<m;j++){		//looping through the sliding window
-                    if(nums[j]>max){		//calculating max value in the sliding window. checking current value is greater than max value. 
-                        max=nums[j];		//storing the max to current value, if  current value is greater than max
-                    }
-                }
-                System.out.print(max+" ");
-            }else {
-            	break;
-            }
-        }
-    }
-    
+            		int max=nums[i];             		 //storing a first value in the sliding window into max
+            		int m=i+k;				//to get sliding window width at each iteration
+            		if(m<=nums.length){				
+                		for(int j=i+1;j<m;j++){		//looping through the sliding window
+                    			if(nums[j]>max){	//calculating max value in the sliding window. checking current value is greater than max value. 
+                        			max=nums[j];	//storing the max to current value, if  current value is greater than max
+                    			}
+                		}
+				System.out.print(max+" ");
+            		}else {
+            			break;
+            		}
+        	}
+    	}
+    	
 	public static void main(String[] args) {
-        
-	    Slidingwindow s=new Slidingwindow();
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();				//reads the array size
-        int[] nums=new int[n];			//Initialization
-        for(int i=0;i<n;i++){
-            nums[i]=sc.nextInt();		//read array values
-        }
-        int k=sc.nextInt();				//read window width
-        s.maxSlidingwindow(nums,k);		//call the function to perform operation
+        	Slidingwindow s=new Slidingwindow();
+        	Scanner sc=new Scanner(System.in);
+        	int n=sc.nextInt();			//reads the array size
+        	int[] nums=new int[n];			//Initialization
+        	for(int i=0;i<n;i++){
+            		nums[i]=sc.nextInt();		//read array values
+        	}
+        	int k=sc.nextInt();			//read window width
+        	s.maxSlidingwindow(nums,k);		//call the function to perform operation
 	}
 }
