aed30eea0d98ad6319647869cb97e2145f2c6e2e
Yashoda k <143180882+Yashuk-11@users.noreply.github.com>
2023-12-05 15:21:16 +0530

    first commit

--- Arraycount.java
+++ Arraycount.java
@@ -5,22 +5,21 @@ import java.util.Scanner;
 public class Arraycount {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-	    int n=sc.nextInt();							//read array size
-	    ArrayList<Integer> a =new ArrayList<>();	//creating a array list
-	    for (int i = 0; i< n; i++) {
-	    	a.add(sc.nextInt());				//read input
-	    }
-
-	    Collections.sort(a);			//sort array in ascending order
-	    int b=a.size()/3;			//find the condition or limit
-	    int i=0;
-        while(i<n) {
-        	int cnt=Collections.frequency(a, a.get(i));  //finding frequency of each value in array
-	        if(cnt>b) {
-	        	System.out.print(a.get(i)+" ");        //print the value if number occurs more than the limit or condition
-	        }
-	        i=i+cnt;            			//increment the index for the next value
-	   }
+	    	int n=sc.nextInt();				//read array size
+	    	ArrayList<Integer> a =new ArrayList<>();	//creating a array list
+	    	for (int i = 0; i< n; i++) {
+	    		a.add(sc.nextInt());			//read input
+	    	}
+		Collections.sort(a);				//sort array in ascending order
+	    	int b=a.size()/3;				//find the condition or limit
+	    	int i=0;
+        	while(i<n) {
+        		int cnt=Collections.frequency(a, a.get(i));  //finding frequency of each value in array
+	        	if(cnt>b) {
+	        		System.out.print(a.get(i)+" ");        //print the value if number occurs more than the limit or condition
+	        	}
+	        	i=i+cnt;            				//increment the index for the next value
+	   	}
 	}
 }
 
